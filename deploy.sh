9bcd886c540fd41b5a2174d052bbb6238ad93bb7
d-mart <dm@dmartinez.net>
2014-09-27 16:18:43 -0400

    Add some github repo pulling to dotfile deploy
    * oh-my-zsh and bash-it as defaults for now

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,15 @@
 # Install / copy repository dotFiles etc
 
 # deploy these files and dirs.
+# @todo - automate generation of this list.
 fileList=".bashrc .bash_aliases .bash_profile prompt.sh .gitconfig .gitexcludes .screenrc .screenrc.infotainment .inputrc .ackrc .Xdefaults .calcrc .gdbinit .irbrc .rbrc .pryrc .tmux.conf"
-dirList=".gdb .mlocate .sh.d"
+dirList=".gdb .mlocate .sh.d .rb.d"
+
+# fetch these repositories
+# Hash where the key is the target directory and the value is the git url
+# e.g.  repos["foo"]="https://github.com/bar/foo"
+declare -A repos=( ["~/.oh-my-zsh"]="https://github.com/robbyrussell/oh-my-zsh"
+                   ["~/.bash-it"]="https://github.com/revans/bash-it")
 
 # Set up "from" and "to" variables
 srcDir=$(cd `dirname "${BASH_SOURCE[0]}"` && pwd)
@@ -12,8 +19,7 @@ targetDir="$HOME"
 # for testing
 #targetDir="/tmp/test"; mkdir -p $targetDir &> /dev/null
 
-for file in $fileList
-do
+for file in $fileList; do
     # make a backup of existing dotFiles
     if [ -f $targetDir/$file ]
     then
@@ -40,4 +46,11 @@ for dir in $dirList; do
     pushd $targetDir > /dev/null
     ln -s $srcDir/$dir $dir
     popd > /dev/null
+done
+
+# git repos
+for target_dir in "${!repos[@]}"; do
+    repo=${repos["$target_dir"]}
+    echo "cloning ${repo} to ${target_dir}"
+    echo git clone --recursive "${repo}" "${target_dir}"
 done
