73785952a112776ee686bcbd194987d87243490c
d-mart <dm@dmartinez.net>
2014-10-02 09:19:57 -0400

    change single quotes to double for alias def (zsh)

--- .sh.d/ruby-setup.sh
+++ .sh.d/ruby-setup.sh
@@ -5,39 +5,39 @@ bundle_exec="bundle exec"
 # bundle_exec="BUNDLE_GEMFILE=\"Gemfile.dm\" bundle exec"
 
 ## general aliases for ruby
-alias be='$bundle_exec'
-alias ber='$bundle_exec rake'
-alias rsp='LOG_LEVEL=DEBUG $bundle_exec rspec -f doc'
-alias cu='$bundle_exec cucumber -x'
-alias cuke='cu'
-alias rs='$bundle_exec rails server'
-alias rc='$bundle_exec rails console'
+alias be="$bundle_exec"
+alias ber="$bundle_exec rake"
+alias rsp="LOG_LEVEL=DEBUG $bundle_exec rspec -f doc"
+alias cu="$bundle_exec cucumber -x"
+alias cuke="cu"
+alias rs="$bundle_exec rails server"
+alias rc="$bundle_exec rails console"
 
 ## daemon control
 # daemon status
-alias dstat='$bundle_exec rake daemons:status'
+alias dstat="$bundle_exec rake daemons:status"
 # dev mode
-alias dstop='$bundle_exec rake daemons:stop'
-alias dstart='$bundle_exec rake daemons:start'
-alias drestart='dstop; dstart'
-alias dbounce='drestart'
+alias dstop="$bundle_exec rake daemons:stop"
+alias dstart="$bundle_exec rake daemons:start"
+alias drestart="dstop; dstart"
+alias dbounce="drestart"
 # test mode
-alias tstart='$bundle_exec rake daemons:test:start'
-alias tstop='$bundle_exec rake daemons:test:stop'
-alias trestart='tstop; tstart'
-alias tbounce='trestart'
+alias tstart="$bundle_exec rake daemons:test:start"
+alias tstop="$bundle_exec rake daemons:test:stop"
+alias trestart="tstop; tstart"
+alias tbounce="trestart"
 # resque
-alias rstop='$bundle_exec rake daemons:resque:stop'
-alias rstart='$bundle_exec rake daemons:resque:start'
+alias rstop="$bundle_exec rake daemons:resque:stop"
+alias rstart="$bundle_exec rake daemons:resque:start"
 # thingling
-alias thingstop='$bundle_exec rake daemons:thingling:stop'
-alias thingstart='$bundle_exec rake daemons:thingling:start'
+alias thingstop="$bundle_exec rake daemons:thingling:stop"
+alias thingstart="$bundle_exec rake daemons:thingling:start"
 
 ## database
 # edit last migration file
-alias elm='ecl db/migrate/`ls -tr db/migrate | tail -1`'
+alias elm="ecl db/migrate/`ls -tr db/migrate | tail -1`"
 # prepare test database
-alias dbtp='$bundle_exec rake db:migrate db:test:prepare'
+alias dbtp="$bundle_exec rake db:migrate db:test:prepare"
 
 ## serve current directoryv via HTTP
 function serve {
