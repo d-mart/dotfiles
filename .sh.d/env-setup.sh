07c17d5770ff15a2a04a2774f1857f1c7dfb643c
d-mart <dm@dmartinez.net>
2015-03-15 20:02:45 -0400

    Colorful manpages

--- .sh.d/env-setup.sh
+++ .sh.d/env-setup.sh
@@ -18,3 +18,17 @@ export SELENIUM_PROFILE_FOR_FIREFOX="selenium"
 # Add local dirs to library paths
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:/usr/local/lib/pkgconfig
+
+# Colorful manpages
+export GROFF_NO_SGR=1
+function man()
+{
+    env LESS_TERMCAP_mb=$'\E[01;31m'   \
+    LESS_TERMCAP_md=$'\E[01;38;5;74m'  \
+    LESS_TERMCAP_me=$'\E[0m'           \
+    LESS_TERMCAP_se=$'\E[0m'           \
+    LESS_TERMCAP_so=$'\E[38;5;246m'    \
+    LESS_TERMCAP_ue=$'\E[0m'           \
+    LESS_TERMCAP_us=$'\E[04;38;5;146m' \
+    man "$@"
+}
