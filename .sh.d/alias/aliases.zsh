96c7f7f3f4ddad94c883e86773a8cf6710cad117
d-mart <dm@dmartinez.net>
2015-03-15 20:02:14 -0400

    Alias additions

--- .sh.d/alias/aliases.zsh
+++ .sh.d/alias/aliases.zsh
@@ -1,1 +1,9 @@
 #!/usr/bin/env zsh
+
+alias -g G='| grep -i' # ignore case
+alias -g L='| less --chop-long-lines'
+alias -g C='| wc -l'
+alias -g H='| head -n 25'
+
+# use 'git' completions also for 'g'
+compdef g='git'
