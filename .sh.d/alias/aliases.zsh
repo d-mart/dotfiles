241919ad7128f97ed58d930e325a9b0cad1c2da9
d-mart <dm@dmartinez.net>
2015-07-29 11:13:24 -0400

    pipe alias for removing newlines #zsh

--- .sh.d/alias/aliases.zsh
+++ .sh.d/alias/aliases.zsh
@@ -4,6 +4,6 @@ alias -g G='| grep -i' # ignore case
 alias -g L='| less --chop-long-lines'
 alias -g C='| wc -l'
 alias -g H='| head -n 25'
-
+alias -g chomp="| tr -d '\r\n'"
 # use 'git' completions also for 'g'
 compdef g='git'
