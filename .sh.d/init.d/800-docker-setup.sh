f3fe3ac8d080c148a73a79818a259f45be6c50e9
d-mart <dm@dmartinez.net>
2017-05-25 08:05:43 -0400

    docker aliases

--- .sh.d/init.d/800-docker-setup.sh
+++ .sh.d/init.d/800-docker-setup.sh
@@ -4,3 +4,10 @@ alias dps='docker ps'
 alias dpsa='docker ps -a'
 alias db='docker build .'
 alias drl='docker run --rm -it $(docker images -q | head -1)'
+
+alias dc='docker-compose'
+alias dm='docker-machine'
+
+
+# TODO Maybe helps tmux run
+# docker run -i -t ... sh -c "exec >/dev/tty 2>/dev/tty </dev/tty && /usr/bin/screen -s /bin/bash"
