d4b20236cc4130ea7139eeb000effce931d45778
d-mart <dm@dmartinez.net>
2019-05-01 18:02:40 -0400

    make mac setup script a little more tidy

--- mac-setup.sh
+++ mac-setup.sh
@@ -17,6 +17,67 @@ function mkdir_with_ln() {
   fi
 }
 
+declare -a brewlist=(
+  "ag"
+  "bash"
+  "bat"
+  "coreutils"
+  "diff-so-fancy"
+  "fasd"
+  "fd"
+  "fzf"
+  "git"
+  "jq"
+  "openssh"
+  "pwgen"
+  "sirens"
+  "tmux"
+  "tree"
+  "vim"
+  "zsh"
+)
+
+declare -a brewcasklist=(
+  "alfred"
+  "appcleaner"
+  "betterzip"
+  "caffeine"
+  "docker"
+  "dropbox"
+  "emacs"
+  "firefox"
+  "flux"
+  "font-anonymous-pro"
+  "font-dejavu-sans-mono-for-powerline"
+  "font-droidsansmono-nerd-font font-droid-sans-mono-for-powerline"
+  "font-inconsolata font-inconsolata-for-powerline"
+  "font-liberation-sans"
+  "font-liberationmono-nerd-font font-liberation-mono-for-powerline"
+  "font-meslo-lg font-input"
+  "font-meslo-lg"
+  "font-nixie-one"
+  "font-office-code-pro"
+  "font-pt-mono"
+  "font-raleway font-roboto"
+  "font-source-code-pro font-source-code-pro-for-powerline"
+  "font-source-sans-pro"
+  "google-chrome"
+  "hammerspoon"
+  "insomnia"
+  "iterm2"
+  "qlcolorcode"
+  "qlmarkdown"
+  "qlprettypatch"
+  "qlstephen"
+  "quicklook-csv"
+  "quicklook-json"
+  "slack"
+  "spectacle"
+  "superduper"
+  "suspicious-package"
+  "vlc"
+  "webpquicklook"
+)
 
 ## make some directories
 mkdir_with_ln "~/util" "~/u"
@@ -34,79 +95,18 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 mkdir ~/screenshots
 defaults write com.apple.screencapture location ~/screenshots/
 
-killall Dock
-killall SystemUIServer
-
 ## Install homebrew
 usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 brew tap caskroom/cask
-
-## Install Casks
-brew cask install \
-    qlcolorcode \
-    qlstephen \
-    qlmarkdown \
-    quicklook-json \
-    qlprettypatch \
-    quicklook-csv \
-    betterzip \
-    webpquicklook \
-    suspicious-package
-
-brew cask install \
-    alfred \
-    appcleaner \
-    caffeine \
-    docker \
-    dropbox \
-    emacs \
-    firefox \
-    hammerspoon \
-    insomnia \
-    flux \
-    slack \
-    spectacle \
-    superduper \
-    vlc
-
-brew cask install google-chrome
-
-## iTerm2 etc
-brew cask install iterm2
 brew tap caskroom/fonts
-brew cask install \
-  font-anonymous-pro \
-  font-dejavu-sans-mono-for-powerline \
-  font-droidsansmono-nerd-font font-droid-sans-mono-for-powerline \
-  font-meslo-lg font-input \
-  font-inconsolata font-inconsolata-for-powerline \
-  font-liberationmono-nerd-font font-liberation-mono-for-powerline \
-  font-liberation-sans \
-  font-meslo-lg \
-  font-nixie-one \
-  font-office-code-pro \
-  font-pt-mono \
-  font-raleway font-roboto \
-  font-source-code-pro font-source-code-pro-for-powerline \
-  font-source-sans-pro
-# todo: install profile
 
-## shell
-brew install git
-brew install coreutils
-brew install openssh
-brew install zsh
-brew install bash
-brew install fzf
-brew install fd
-brew install tree
-brew install tmux
-brew install ag
-brew install bat
-brew install sirens
-brew install jq
-brew install pwgen
-brew install diff-so-fancy
+for package in "${brewlist[@]}"; do
+  brew install "$package"
+done
+
+for cask in "${brewcasklist[@]}"; do
+  brew cask install "$cask"
+done
 
 # oh-my-zsh
 git clone https://github.com/robbyrussell/oh-my-zsh ~/.oh-my-zsh
@@ -116,18 +116,16 @@ git clone ssh://git@gitlab.dmartinez.net:61222/dmartinez/dotfiles.git ~/personal
 ( cd ~/personal/dotfiles ; ./deploy.sh )
 
 ## vim (just in case)
-brew install vim
 git clone https://github.com/amix/vimrc.git ~/.vim_runtime
 sh ~/.vim_runtime/install_awesome_vimrc.sh
 
 ## emacs
-brew cask install emacs
 git clone https://github.com/cask/cask ~/.cask
 git clone ssh://git@gitlab.dmartinez.net:61222/dmartinez/dotfiles.git ~/personal/dotemacs
 ln -s ~/personal/dotemacs ~/.emacs.d/
 ( cd ~/.cask ; bin/cask install )
 # Yes, it takes repeated tries for some reason.
-( cd ~/.emacs.d ; ~/.cask/bin/cask install ; ~/.cask/bin/cask install ; ~/.cask/bin/cask install ; )
+( cd ~/.emacs.d ; ~/.cask/bin/cask install ; ~/.cask/bin/cask install ; ~/.cask/bin/cask install )
 
 ## versions of stuff
 brew install asdf
@@ -138,5 +136,9 @@ asdf plugin-add python https://github.com/danhper/asdf-python.git
 asdf plugin-add nodejs https://github.com/asdf-vm/asdf-nodejs.git
 asdf plugin-add rust https://github.com/code-lever/asdf-rust
 
-## requires password / interaction
+## Restart some things
+killall Dock
+killall SystemUIServer
+
+## requires password / interaction (but doesn't seem to work anymore)
 chsh -s $(which zsh) $(whoami)
