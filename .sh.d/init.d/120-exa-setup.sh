5050678990d523591768ab92cfac7d6e5465b603
David Martinez <dm@dmartinez.net>
2019-05-11 18:53:28 -0400

    Add exa aliases, contigent on its presence

---
+++ .sh.d/init.d/120-exa-setup.sh
@@ -0,0 +1,8 @@
+if (hash exa 2> /dev/null ) ; then
+  alias ll="exa --long --all --sort=modified"
+  alias la="exa --long --all --sort=modified"
+  alias l2="exa --long --all --tree --level=2 --sort=modified --color-scale"
+  alias l3="exa --long --all --tree --level=3 --sort=modified --color-scale"
+  alias ln="exa --long --all --sort=name"
+  alias lt="exa --long --all --sort=extension"
+fi
