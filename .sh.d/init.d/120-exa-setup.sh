4677f644b3072824b3d5f7043fe01460d47ac21c
David Martinez <dm@dmartinez.net>
2019-05-14 18:02:50 -0400

    Don't clobber 'ln' with an alias for exa

--- .sh.d/init.d/120-exa-setup.sh
+++ .sh.d/init.d/120-exa-setup.sh
@@ -3,6 +3,6 @@ if (hash exa 2> /dev/null ) ; then
   alias la="exa --long --all --sort=modified"
   alias l2="exa --long --all --tree --level=2 --sort=modified --color-scale"
   alias l3="exa --long --all --tree --level=3 --sort=modified --color-scale"
-  alias ln="exa --long --all --sort=name"
+  alias lln="exa --long --all --sort=name"
   alias lt="exa --long --all --sort=extension"
 fi
