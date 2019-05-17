6effbf2749dd30c4c2f360ebc10e9df46d8e7b00
David Martinez <dm@dmartinez.net>
2019-05-17 14:04:09 -0400

    Never again look up how to list open ports on macOS

--- .sh.d/init.d/400-gen-utils.sh
+++ .sh.d/init.d/400-gen-utils.sh
@@ -9,3 +9,20 @@ function mkcd() {
   dir="$1"
   mkdir -p "$dir" && cd "$dir"
 }
+
+#
+# list listening ports
+#
+if [ "$OS" = "mac" ]; then
+  function lports() {
+    lsof -nP -i4TCP | grep LISTEN
+  }
+elif [ "$OS" = "linux" ]; then
+  function lports() {
+    ss -tnl
+  }
+else
+  function lports() {
+    echo "unknown operation system"
+  }
+fi
