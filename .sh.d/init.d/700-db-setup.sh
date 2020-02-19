2bb6a16356a1b966060304cfeaa2730bfe5e7ec7
David Martinez <dm@dmartinez.net>
2020-02-19 16:42:34 -0500

    Add iredis wrapper func

--- .sh.d/init.d/700-db-setup.sh
+++ .sh.d/init.d/700-db-setup.sh
@@ -5,3 +5,12 @@ function pc() {
   setting_name="${2:-DATABASE_URL}"
   pgcli $(heroku config:get "$setting_name" --app="$heroku_app")
 }
+
+function ir() {
+  iredis="$(asdf where python)/bin/iredis"
+  host="${1:-127.0.0.1}"
+  port="${2:-6379}"
+  db_num="${3:-0}"
+
+  $iredis --rainbow -h "$host" -p "$port" -n "$db_num"
+}
