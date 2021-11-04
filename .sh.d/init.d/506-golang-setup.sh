c060c86d51b300bb7411352659775b6a5ff649d5
David Martinez <dm@dmartinez.net>
2021-11-04 16:01:32 -0400

    Update golang env boilerplate

--- .sh.d/init.d/506-golang-setup.sh
+++ .sh.d/init.d/506-golang-setup.sh
@@ -1,4 +1,9 @@
 # shell environment setup for golang
+GOPATH="$HOME/go"
+PATH="$PATH:$GOPATH/bin"
 
 # adapted from https://github.com/kennyp/asdf-golang/issues/28
 alias go-reshim='asdf reshim golang && export GOROOT="$(asdf where golang)/go"'
+
+# Note to self
+# go get github.com/pcarrier/gauth
