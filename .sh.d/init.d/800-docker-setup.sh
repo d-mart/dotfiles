9f8dcd06e80afa0f34dab3c0ed116f6dd54a48c0
David Martinez <dmartinez@dm.tld>
2025-04-09 14:07:58 -0400

    Add podman (and detection) to docker setup for use instead

--- .sh.d/init.d/800-docker-setup.sh
+++ .sh.d/init.d/800-docker-setup.sh
@@ -1,3 +1,15 @@
+## docker, podman, other container-related tool and config setup
+
+function set_docker_host() {
+  local readonly podman_socket=$(podman machine inspect --format '{{ .ConnectionInfo.PodmanSocket.Path }}')
+  export DOCKER_HOST="$podman_socket"
+}
+
+if command -v podman &> /dev/null; then
+  alias docker=podman
+  set_docker_host
+fi
+
 alias d='docker'
 alias di='docker images'
 alias dps='docker ps'
@@ -6,13 +18,10 @@ alias db='docker build .'
 alias dr='docker run --rm --interactive --tty'
 alias drl='docker run --rm --interactive --tty $(docker images -q | head -1)'
 
-alias dc='docker-compose'
-alias dce='docker-compose exec'
-function dck() { docker-compose stop "$1" ; docker-compose rm -f "$1" }
+alias dc='docker compose'
+alias dce='docker compose exec'
+function dck() { docker compose stop "$1" ; docker compose rm -f "$1" }
 
 # if needed...
 #export DOCKER_CLIENT_TIMEOUT=120
 #export COMPOSE_HTTP_TIMEOUT=120
-
-# TODO Maybe helps tmux run
-# docker run -i -t ... sh -c "exec >/dev/tty 2>/dev/tty </dev/tty && /usr/bin/screen -s /bin/bash"
