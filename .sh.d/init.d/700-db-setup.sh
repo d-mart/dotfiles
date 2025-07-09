344858d1022c31c330b0f591217ade93c29b8dab
David Martinez <dmartinez@dm.tld>
2025-07-09 18:54:48 -0400

    Cleanup on unused/moved aliases

--- .sh.d/init.d/700-db-setup.sh
+++ .sh.d/init.d/700-db-setup.sh
@@ -1,11 +1,4 @@
 
-# run with a heroku app name to connect to the database
-function pc() {
-  heroku_app="$1"
-  setting_name="${2:-DATABASE_URL}"
-  pgcli $(heroku config:get "$setting_name" --app="$heroku_app")
-}
-
 function ir() {
   iredis="$(asdf where python)/bin/iredis"
   host="${1:-127.0.0.1}"
