77b5c0edb4a60605a07c8af67ccbba06d373ce9f
David Martinez <dmartinez@dm.tld>
2025-11-17 02:48:39 +0000

    Update setup for newer non-bash asdf

--- .sh.d/init.d/500-asdf-setup.sh
+++ .sh.d/init.d/500-asdf-setup.sh
@@ -1,10 +1,12 @@
 # asdf programming language version manager
 
-# typical linux manual installation
-if [ -f "$HOME/.asdf/asdf.sh" ] ; then 
+# asdf setup - supports both Go-based (newer) and bash-based versions
+# remove the second branch eventually, around mid 2026, and update all 
+# systems to have > 0.18
+if command -v asdf >/dev/null 2>&1 && [ ! -f "$HOME/.asdf/asdf.sh" ]; then
+  # New Go-based asdf
+  export PATH="$HOME/.asdf/shims:$PATH"
+elif [ -f "$HOME/.asdf/asdf.sh" ]; then
+  # Old bash-based asdf
   . "$HOME/.asdf/asdf.sh"
-
-  # TODO bash --> . $HOME/.asdf/completions/asdf.bash
-  # TODO zsh  --> fpath=(${ASDF_DIR}/completions $fpath)
-  #               autoload -Uz compinit && compinit
 fi
